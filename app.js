0d18818b9adc3b4e64593df241744fdac7f4f187
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-10-10 12:58:42 -0400

    test

---
+++ app.js
@@ -0,0 +1,40 @@
+const lego = [
+
+    {
+        name: "Brick1",
+        size: "1x2",
+        Color: "Blue",
+        set_appeared_in: ['102' ,'104' , '105' ,'109']
+    },
+
+    {
+        name: "Brick2",
+        size: "2x2",
+        Color: "Red",
+        set_appeared_in: ['102' ,'103' , '101' ,'99']
+    },
+
+    {
+        name: "Brick3",
+        size: "4x2",
+        Color: "Yellow",
+        set_appeared_in: ['167' ,'67' , '175' ,'75']
+    },
+
+    {
+        name: "Brick4",
+        size: "4x4",
+        Color: "Green",
+        set_appeared_in: ['90 ','34' , '134' ,'275']
+    },
+
+]
+
+lego.forEach((lego) => console.log(lego.name))
+
+
+lego.forEach((lego) => console.log(lego.set_appeared_in) )
+
+
+
+
